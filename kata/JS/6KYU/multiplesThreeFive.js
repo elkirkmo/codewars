6276d56b0b0968b958d7d05fd952f13f09ea83da
Chris Kirkham <ckirkham@fandango.com>
2017-03-09 18:59:46 -0700

    Updated multiplesThreeFive.js with revised code.

--- kata/JS/6KYU/multiplesThreeFive.js
+++ kata/JS/6KYU/multiplesThreeFive.js
@@ -2,12 +2,8 @@ function solution(number){
 let sum = 0;
 	for (i = 1; i < number; i++){
 		if (i % 3 === 0){
-    	console.log(sum)
-      console.log(i);
     	sum = sum + i;
     }else if( i % 5 === 0){
-				console.log(sum)
-      console.log(i);
     	sum = sum + i;
   	} else {
     	continue;

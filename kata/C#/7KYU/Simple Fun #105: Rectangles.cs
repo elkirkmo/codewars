eb71c60e2d899bc7b5a65b4d0708a0b39f6cdcf5
Chris Kirkham <ckirkham@fandango.com>
2017-03-27 13:30:32 -0600

    Simple Fun #105: Rectangles in C# and JS

---
+++ kata/C#/7KYU/Simple Fun #105: Rectangles.cs
@@ -0,0 +1,11 @@
+namespace myjinxin
+{
+    using System;
+    
+    public class Kata
+    {
+        public int Rectangles(int n, int m){
+          return (n-1)*(m-1)*n*m/4;
+        }
+    }
+}
---
+++ kata/JS/7KYU/Simple Fun #105: Rectangles.js
@@ -0,0 +1,3 @@
+function rectangles(n, m) {
+  return (n-1)*(m-1)*n*m/4
+}

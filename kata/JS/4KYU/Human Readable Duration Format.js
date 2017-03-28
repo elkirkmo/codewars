db43902c158f7813761db7235b480d3ff383dd9c
Chris Kirkham <ckirkham@fandango.com>
2017-03-28 16:15:04 -0600

    Added .eslintrc.js

--- kata/JS/4KYU/Human Readable Duration Format.js
+++ kata/JS/4KYU/Human Readable Duration Format.js
@@ -1,32 +1,38 @@
 function formatDuration (seconds) {
-  if (seconds === 0)
+  if (seconds === 0) {
     return 'now';
-  let years =0, months=0, days=0, hours=0, minutes=0, secs = 0;  
-  while (seconds > 59) {
-    minutes++
-    seconds -= 60
   }
-   secs = seconds
-   while (minutes > 59) {
-     hours++
-    minutes -= 60
-   }
-   while (hours > 23){
-     days++
-    hours -= 24
-   }
-   while (days > 364) {
-     years++
-    days -=365;
-   }
-   let nums = {"years":years, "months":months, "days":days, "hours" : hours, "minutes" :minutes, "seconds": secs};
-   let str = '';
-   for (let v in nums) {
-     nums[v] === 0 ? '' : str += nums[v] + " "+(nums[v] > 1 ? v : v.replace(/s$/g,"")) + ", ";
-   }
-   str = str.trim().replace(/,$/g,"");
-   if (str.indexOf(',') > -1 ){
-     return str.replace(/,([^,]*)$/,'\ and$1');
-   } else 
-     return str;
+  let years = 0;
+  let days = 0;
+  let hours = 0;
+  let minutes = 0;
+  let secs = 0;
+  while (seconds > 59) {
+    minutes++;
+    seconds -= 60;
+  }
+  secs = seconds;
+  while (minutes > 59) {
+    hours++;
+    minutes -= 60;
+  }
+  while (hours > 23) {
+    days++;
+    hours -= 24;
+  }
+  while (days > 364) {
+    years++;
+    days -= 365;
+  }
+  const nums = { 'years': years, 'months': months, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': secs};
+  let str = '';
+  
+  for (let v in nums) {
+    nums[v] === 0 ? '' : str += nums[v] + ' ' + (nums[v] > 1 ? v : v.replace(/s$/g,'')) + ', ';
+  }
+  str = str.trim().replace(/,$/g, '');
+  if (str.indexOf(',') > -1) {
+    return str.replace(/,([^,]*)$/, ' and$1');
+  }
+  return str;
 }

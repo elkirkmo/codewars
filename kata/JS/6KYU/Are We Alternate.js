ca73cb42210a8c33b3d16fbd65a9826adab77562
Chris Kirkham <ckirkham@fandango.com>
2017-07-19 11:43:50 -0600

    daily kata 20170719

---
+++ kata/JS/6KYU/Are We Alternate.js
@@ -0,0 +1,14 @@
+function isAlt(word) {
+  const a = word.split('');
+  for (let i = /[aeiou]/g.test(a[0]) ? 0 : 1; i < a.length; i += 2) {
+    if (a.length === 1) {
+      return true;
+    }
+    if (!/[aeiou]/g.test(a[i])) {
+      return false;
+    } else if (/[aeiou]/g.test(a[i]) && /[aeiou]/g.test(a[i + 1]) && i + 1 < a.length) {
+      return false;
+    }
+  }
+  return true;
+}
